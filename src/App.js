1c190089999b8b668810020a8e4cf4a684c068f5
Momo Rao <monicaxrao@gmail.com>
2018-09-05 22:25:48 -0400

    fix basename in app.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
 
     render() {
         return (
-        	<BrowserRouter>
+        	<BrowserRouter basename={process.env.PUBLIC_URL}>
 	        <div>
 	        <Route exact path="/" render={() => (
 	          <ListContacts 
